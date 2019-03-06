7e0b3d3003f054b43594a1a66c08d0e7b75bdb69
Nicolás Hernández <nm.hernandez10@uniandes.edu.co>
2019-03-06 11:00:29 -0500

    ratings man

--- webpage/artistfm/src/app/components/userComponents/UserSearch.js
+++ webpage/artistfm/src/app/components/userComponents/UserSearch.js
@@ -42,10 +42,10 @@ class UserSearch extends Component{
     }
     else if(value !== ''){
       fetch('/api/artist/search/'+value.trim()).then(res=>res.json()).then(data =>{
-        console.log
+        console.log(data);
         this.setState({
           toptensearchrates: []
-        }, () =>{
+        }, (data) =>{
           for(artist in data){
             artist_id = data[artist].id;
             var rate = this.props.ratings.filter(function(rating){
